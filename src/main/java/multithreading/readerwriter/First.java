84510e48e2e0b7c7e7ad35e926d1983cff5e9aa6
Alex Plate <AlexPl292@gmail.com>
2018-10-10 14:01:40 +0300

    Reformat structure

--- src/main/java/multithreading/readerwriter/First.java
+++ src/main/java/multithreading/readerwriter/First.java
@@ -1,69 +1,43 @@
-package multithreading.readerwriter.second;
+package multithreading.readerwriter;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Created by Alex Plate on 10.10.2018.
+ * Created by Alex Plate on 09.10.2018.
  */
-public class Main {
-
+public class First {
     private static Semaphore resourceLock = new Semaphore(1);
     private static Semaphore readerLock = new Semaphore(1);
-    private static Semaphore writerLock = new Semaphore(1);
-    private static Semaphore readDenyLock = new Semaphore(1);
-
-    private static volatile AtomicInteger writerCount = new AtomicInteger(0);
     private static volatile AtomicInteger readerCount = new AtomicInteger(0);
-
 
     public static void main(String[] args) throws InterruptedException {
         while (true) {
-            Writer writer1 = new Writer();
-            Writer writer2 = new Writer();
-            Writer writer3 = new Writer();
+            Writer writer = new Writer();
             Reader reader1 = new Reader();
             Reader reader2 = new Reader();
             Reader reader3 = new Reader();
 
-            writer1.start();
-
+            writer.start();
             reader1.start();
             reader2.start();
             reader3.start();
-
-            writer2.start();
-            writer3.start();
 
             Thread.sleep(1000);
         }
     }
 
-    private static class Writer extends Thread {
 
+    private static class Writer extends Thread {
         @Override
         public void run() {
             try {
-                writerLock.acquire();
-                writerCount.incrementAndGet();
-                if (writerCount.intValue() == 1) {
-                    readDenyLock.acquire();
-                }
-                writerLock.release();
-
                 resourceLock.acquire();
 
-                Thread.sleep(10);
-                System.out.println("Writing. Writers: " + writerCount);
+                System.out.println("Writing");
+                Thread.sleep(1);
 
                 resourceLock.release();
-
-                writerLock.acquire();
-                writerCount.decrementAndGet();
-                if (writerCount.intValue() == 0) {
-                    readDenyLock.release();
-                }
-                writerLock.release();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -71,21 +45,18 @@ public class Main {
     }
 
     private static class Reader extends Thread {
-
         @Override
         public void run() {
             try {
-                readDenyLock.acquire();
                 readerLock.acquire();
                 readerCount.incrementAndGet();
                 if (readerCount.intValue() == 1) {
                     resourceLock.acquire();
                 }
                 readerLock.release();
-                readDenyLock.release();
 
-                Thread.sleep(10);
                 System.out.println("Read. Readers: " + readerCount);
+                Thread.sleep(1);
 
                 readerLock.acquire();
                 readerCount.decrementAndGet();
--- src/main/java/multithreading/readerwriter/Second.java
+++ src/main/java/multithreading/readerwriter/Second.java
@@ -1,43 +1,69 @@
-package multithreading.readerwriter.first;
+package multithreading.readerwriter;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Created by Alex Plate on 09.10.2018.
+ * Created by Alex Plate on 10.10.2018.
  */
-public class Main {
+public class Second {
+
     private static Semaphore resourceLock = new Semaphore(1);
     private static Semaphore readerLock = new Semaphore(1);
+    private static Semaphore writerLock = new Semaphore(1);
+    private static Semaphore readDenyLock = new Semaphore(1);
+
+    private static volatile AtomicInteger writerCount = new AtomicInteger(0);
     private static volatile AtomicInteger readerCount = new AtomicInteger(0);
+
 
     public static void main(String[] args) throws InterruptedException {
         while (true) {
-            Writer writer = new Writer();
+            Writer writer1 = new Writer();
+            Writer writer2 = new Writer();
+            Writer writer3 = new Writer();
             Reader reader1 = new Reader();
             Reader reader2 = new Reader();
             Reader reader3 = new Reader();
 
-            writer.start();
+            writer1.start();
+
             reader1.start();
             reader2.start();
             reader3.start();
+
+            writer2.start();
+            writer3.start();
 
             Thread.sleep(1000);
         }
     }
 
-
     private static class Writer extends Thread {
+
         @Override
         public void run() {
             try {
+                writerLock.acquire();
+                writerCount.incrementAndGet();
+                if (writerCount.intValue() == 1) {
+                    readDenyLock.acquire();
+                }
+                writerLock.release();
+
                 resourceLock.acquire();
 
-                System.out.println("Writing");
-                Thread.sleep(1);
+                Thread.sleep(10);
+                System.out.println("Writing. Writers: " + writerCount);
 
                 resourceLock.release();
+
+                writerLock.acquire();
+                writerCount.decrementAndGet();
+                if (writerCount.intValue() == 0) {
+                    readDenyLock.release();
+                }
+                writerLock.release();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -45,18 +71,21 @@ public class Main {
     }
 
     private static class Reader extends Thread {
+
         @Override
         public void run() {
             try {
+                readDenyLock.acquire();
                 readerLock.acquire();
                 readerCount.incrementAndGet();
                 if (readerCount.intValue() == 1) {
                     resourceLock.acquire();
                 }
                 readerLock.release();
+                readDenyLock.release();
 
+                Thread.sleep(10);
                 System.out.println("Read. Readers: " + readerCount);
-                Thread.sleep(1);
 
                 readerLock.acquire();
                 readerCount.decrementAndGet();
